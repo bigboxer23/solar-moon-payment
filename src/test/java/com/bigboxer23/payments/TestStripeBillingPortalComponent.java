fbe3d7f691ec09dcd21b4b6acd7728462592d97a
Matt Jones <bigboxer23@gmail.com>
2024-02-28 09:44:14 -0600

    ops: bump lib versions (#31)

---
+++ src/test/java/com/bigboxer23/payments/TestStripeBillingPortalComponent.java
@@ -0,0 +1,18 @@
+package com.bigboxer23.payments;
+
+import com.stripe.exception.StripeException;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ */
+public class TestStripeBillingPortalComponent
+{
+	private StripeBillingPortalComponent component = new StripeBillingPortalComponent();
+
+	@Test
+	public void createCustomerBillingPortal() throws StripeException
+	{
+		component.createCustomerBillingPortal();
+	}
+}
