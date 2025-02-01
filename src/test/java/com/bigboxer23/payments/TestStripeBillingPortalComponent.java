07540d32a346d70d0f88d24718490876f46076d8
Matt Jones <bigboxer23@gmail.com>
2025-02-01 14:49:26 -0600

    feat: add slf4j annotation for standardized logging

--- src/test/java/com/bigboxer23/payments/TestStripeBillingPortalComponent.java
+++ src/test/java/com/bigboxer23/payments/TestStripeBillingPortalComponent.java
@@ -3,16 +3,12 @@ package com.bigboxer23.payments;
 import com.stripe.exception.StripeException;
 import org.junit.jupiter.api.Test;
 
-/**
- *
- */
-public class TestStripeBillingPortalComponent
-{
+/** */
+public class TestStripeBillingPortalComponent {
 	private StripeBillingPortalComponent component = new StripeBillingPortalComponent();
 
 	@Test
-	public void createCustomerBillingPortal() throws StripeException
-	{
+	public void createCustomerBillingPortal() throws StripeException {
 		component.createCustomerBillingPortal();
 	}
 }
